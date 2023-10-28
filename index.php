f6f8ecd3b0ba64bf86419923365f00877a4123ab
Ruggi1999 <lucaruggirello1999@gmail.com>
2023-10-28 15:42:20 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SITO DI PROVA</title>
+
+    <link rel="stylesheet" href="CSS/index.css">
+
+</head>
+
+<body>
+
+<header class="header-001">
+
+        <nav class="nav-001">
+
+            <ul class="ul-001">
+
+                <li class="li-001"><a href="" class="a-001"><i class="fa-solid fa-square-phone fa-lg" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-001"><a href="" class="a-001"><i class="fa-solid fa-envelope fa-lg" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-001"><a href="" class="a-001">Eventi</a></li>
+                
+                <li class="li-001"><a href="" class="a-001">Attività</a></li>
+                
+                <li class="li-001"><a href="" class="a-001">Pensione</a></li>
+                
+                <li class="li-001"><a href="" class="a-001">Allevamento</a></li>
+
+                <li class="li-001"><a href="" class="a-001">Il nostro team</a></li>
+
+                <li class="li-001"><a href="" class="a-001">Addestramento</a></li>
+
+                <li class="li-001"><a href="" class="a-001"><i class="fa-brands fa-square-facebook fa-lg" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-001"><a href="" class="a-001"><i class="fa-brands fa-instagram fa-lg" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-001"><a href="#" class="a-003" id="a-003"><i class="fa-solid fa-equals fa-lg menu" style="color: #ffffff;"></i></a></li>
+            
+            </ul>
+        
+        </nav>
+        
+        <nav class="nav-002">
+
+            <ul class="ul-002">
+
+                <li class="li-002"><a href="" class="a-002"><span>Eventi</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-002"><a href="" class="a-002"><span>Attività</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-002"><a href="" class="a-002"><span>Pensione</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-002"><a href="" class="a-002"><span>Allevamento</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-002"><a href="" class="a-002"><span>Il nostro team</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+                
+                <li class="li-002"><a href="" class="a-002"><span>Addestramento</span><i class="fa-solid fa-chevron-up fa-rotate-90 fa-2xs" style="color: #ffffff;"></i></a></li>
+           
+            </ul>
+        
+        </nav>
+  
+    </header>
+
+    <section class="section-001">
+        
+        <div class="div-001">
+
+            <img src="IMG/sfondo.jpg" alt="Sfondo">
+        
+        </div>
+       
+        <div class="div-002">
+         
+            <img src="IMG/logo.png" alt="Sfondo">
+      
+        </div>
+        
+        <div class="div-003">
+            
+            <a href="">Allevamento</a>
+        
+            <a href="">Attività cinofile</a>
+        
+        </div>
+    
+    </section>
+    
+    <script src="JS/index.js"></script>
+    
+    <script src="https://kit.fontawesome.com/547ed035a5.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
+
